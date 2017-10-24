4170e346a343c57cccfe4a92276a79b13045258a
Luis Tertulino <luistertulino95@gmail.com>
2017-10-23 23:29:59 -0300

    created directional light

---
+++ libs/lights/Directional.h
@@ -0,0 +1,26 @@
+#ifndef DIRECTIONAL_H_
+#define DIRECTIONAL_H_
+#include "../Light.h"
+
+class Directional : public Light{
+  
+  public:
+
+    // Attributes
+    Vector3 direction;
+
+    // Constructors
+    Directional() : Light(){
+        direction = Vector3(1.0,1.0,1.0);
+    }
+    Directional(Vector3 &_direction, RGB &_intensity) : Light(_intensity){
+        source = _source;
+    }
+  
+    // Methods
+    virtual Vector3 get_direction(const Point3 &new_origin) const override;
+}
+
+Vector3 Direction::get_direction(const Point3 &new_origin) const {
+    return direction;
+}
