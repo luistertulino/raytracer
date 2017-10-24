dc96b721ce90e4b3d5383b5436e7cc9e150868ed
Luis Tertulino <luistertulino95@gmail.com>
2017-10-23 22:44:08 -0300

    modified Light class: now only 1 constructor

--- libs/Light.h
+++ libs/Light.h
@@ -7,16 +7,13 @@ class Light{
 
   public:
 
-	// Attributes
-	RGB intensity;
+    // Attributes
+    RGB intensity;
 
     // Constructors
-    Light(){
-		intensity = RGB(1.0,1.0,1.0);
+    Light(RGB _intensity = RGB(1.0,1.0,1.0)){
+        intensity = _intensity;
     }
-	Light(RGB _intensity){
-		intensity = _intensity;
-	}
 
     // Methods
     virtual Vector3 get_direction(const Point3 &new_origin) const = 0;
