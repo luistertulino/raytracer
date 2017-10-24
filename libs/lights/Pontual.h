20a86b7ae9243be2d4856795204061951c2adf52
Luis Tertulino <luistertulino95@gmail.com>
2017-10-23 23:16:17 -0300

    added constructors and get_direction

--- libs/lights/Pontual.h
+++ libs/lights/Pontual.h
@@ -13,6 +13,14 @@ class Pontual : public Light{
     Pontual(Point3 _source = Point3(0.0,0.0,0.0)) : Light(){
         source = _source;
     }
-    Pontual
+    Pontual(Point3 _source, RGB _intensity) : Light(_intensity){
+        source = _source;
+    }
+  
     // Methods
+    virtual Vector3 get_direction(const Point3 &new_origin) const override;
+}
+
+Vector3 Pontual::get_direction(const Point3 &new_origin) const {
+    return source - new_origin;
 }
